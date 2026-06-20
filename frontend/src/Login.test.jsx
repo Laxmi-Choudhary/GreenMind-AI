3c29c181f13b4ae8654f4901ab0c5b64647f906e
Laxmi-Choudhary <23107140@apsit.edu.in>
2026-06-21 03:00:52 +0530

    Login.test.jsx

---
+++ frontend/src/Login.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Login from "../pages/Login";
+
+test("renders login page", () => {
+  render(<Login />);
+
+  expect(
+    screen.getByText(/login/i)
+  ).toBeInTheDocument();
+});
